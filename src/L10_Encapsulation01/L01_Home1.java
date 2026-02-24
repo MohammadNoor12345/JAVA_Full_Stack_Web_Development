b1e883c96dd909e3e5128ee00f0a1fb2030deb85
Mohammad Noor <mohammadnoor76354@gmail.com>
2026-02-24 17:02:27 +0530

    Update1.7

--- src/L10_Encapsulation01/L01_Home1.java
+++ src/L10_Encapsulation01/L01_Home1.java
@@ -4,17 +4,21 @@ class TV {
     private int volume; // Protecting my important components
     private int brightness; // Protecting my important components
 
+
     public int getVolume() {
         return volume;
     }
 
     public void setVolume(int volume) {
-        if(volume <= 50) {
-            this.volume = volume;
-        }
-        else {
-            System.out.println("Volume can,t be greater than 50");
-        }
+//        if(volume <= 50) {
+//            this.volume = volume;
+//        }
+//        else {
+//            System.out.println("Volume can't be greater than 50");
+//        }
+        // Using Ternary Operator
+        this.volume = (volume <= 50) ? volume : this.volume;
+        System.out.println((volume <= 50) ? "" : "Volume can't be greater than 50");
     }
 
     public int getBrightness() {
@@ -22,10 +26,9 @@ class TV {
     }
 
     public void setBrightness(int brightness) {
-        if(brightness <= 60) {
+        if (brightness <= 60) {
             this.brightness = brightness;
-        }
-        else {
+        } else {
             System.out.println("Brightness can't be greater than 60");
         }
     }
@@ -38,9 +41,11 @@ class TV {
 public class L01_Home1 {
     static void main(String[] args) {
         TV t = new TV();
+
         t.setVolume(50);
         t.setBrightness(60);
         System.out.println(t.getVolume());
         System.out.println(t.getBrightness());
     }
 }
+

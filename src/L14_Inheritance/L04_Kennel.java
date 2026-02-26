433f0875790aa13ee2aad06d3db42996d3c55d3f
Mohammad Noor <mohammadnoor76354@gmail.com>
2026-02-26 11:55:15 +0530

    Update2.3

--- src/L14_Inheritance/L04_Kennel.java
+++ src/L14_Inheritance/L04_Kennel.java
@@ -1,12 +1,41 @@
 package L14_Inheritance;
 
 class Dog {
+
+    String name;
+    int cost;
+
+    public Dog(String name) {
+        System.out.println("Name Constructor");
+        this.name = name;
+    }
+
+    public Dog(int cost) {
+        this("Ruby");
+        System.out.println("Cost Constructor");
+        this.cost = cost;
+    }
+
+    public Dog(String name, int cost) {
+        this.name = name;
+        this.cost = cost;
+    }
+
+    public  Dog() {
+        this(1000);
+        System.out.println("Dog Object is created");
+    }
+
     public void bark() {
         System.out.println("Dog is Barking");
     }
 }
 
 class Puppy extends Dog {
+    public Puppy() {
+        super();
+        System.out.println("Puppy object created");
+    }
 
 }
 

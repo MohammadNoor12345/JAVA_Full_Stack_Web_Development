c896a2c8f7ace39b88e16cdbd11cce094d36e662
Mohammad Noor <mohammadnoor76354@gmail.com>
2026-03-02 17:19:55 +0530

    Update-3.2

--- src/L16_Abstraction/L05_MyLab.java
+++ src/L16_Abstraction/L05_MyLab.java
@@ -1,4 +1,41 @@
 package L16_Abstraction;
 
+class Calci {
+
+    public void add() {
+        int a = 20;
+        int b = 30;
+        System.out.println("a + b = " + (a + b));
+    }
+
+    public void sub() {
+        int a = 100;
+        int b = 50;
+        System.out.println("a - b = " + (a - b));
+    }
+
+    public void mul() {
+        int a = 10;
+        int b = 5;
+        System.out.println("a * b = " + (a * b));
+    }
+
+    public void div() {
+        int a = 40;
+        int b = 8;
+        System.out.println("a / b = " + (a / b));
+    }
+}
+
 public class L05_MyLab {
+
+    public static void main(String[] args) {
+
+        Calci c = new Calci();
+
+        c.add();
+        c.sub();
+        c.mul();
+        c.div();
+    }
 }

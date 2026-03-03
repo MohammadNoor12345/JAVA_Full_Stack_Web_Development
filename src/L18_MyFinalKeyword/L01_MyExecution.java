f6f505542838c3031e172d95fc7815384633f061
Mohammad Noor <mohammadnoor76354@gmail.com>
2026-03-03 11:21:04 +0530

    Update-3.5

--- src/L18_MyFinalKeyword/L01_MyExecution.java
+++ src/L18_MyFinalKeyword/L01_MyExecution.java
@@ -1,4 +1,8 @@
 package L18_MyFinalKeyword;
+/*final class MyFinal{
+
+}*/
+// final class can't be extended
 class MyFinal {
     public void nonfinal() {
         // Normal method → can be overridden
@@ -10,12 +14,14 @@ class MyFinal {
         System.out.println("m1() is a final method which can't be overridden");
     }
 }
-class  MyChild extends MyFinal {
+class MyChild extends MyFinal {
     @Override
     public void nonfinal() {
         // ✅ Allowed because parent method is not final
         System.out.println("Overriding non final in child class");
     }
+
+
  /*   @Override
     public final void m1() {
 
@@ -24,15 +30,32 @@ class  MyChild extends MyFinal {
         // If you try, compiler error:
         // "Cannot override the final method from MyFinal"
     }*/
+
+/* public abstract final void m2();
+ // Abstract compulsory overrides
+    // final can't be overrides*/
 }
 
 public class L01_MyExecution {
 
     public static void main(String[] args) {
-        // main must be public static for JVM to execute
 
-        MyChild c = new MyChild();
+/*        MyChild c = new MyChild();
         c.nonfinal();  // Calls overridden version
-        c.m1();        // Calls parent version (cannot be overridden)
+        c.m1();        // Calls parent version (cannot be overridden)*/
+
+        double A = 100.01;
+        System.out.println(A);
+
+        A = 100.09;
+        // ✅ Allowed because A is NOT final
+        // Normal variables can be reassigned
+
+        System.out.println(A);
+
+        System.out.println(Math.PI);
+        // ✅ Math.PI is a static final double constant
+        // It belongs to Math class
+        // It cannot be modified
     }
 }

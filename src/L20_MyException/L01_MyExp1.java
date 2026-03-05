04746705da69ee1429e67ff960706beceb3a6650
Mohammad Noor <mohammadnoor76354@gmail.com>
2026-03-05 15:46:16 +0530

    Update-5.0

--- src/L20_MyException/L01_MyExp1.java
+++ src/L20_MyException/L01_MyExp1.java
@@ -6,9 +6,13 @@ public class L01_MyExp1 {
     static void main(String[] args) {
         System.out.println("Connection Established...");
         Scanner scan = new Scanner(System.in);
-        System.out.println("Enter Your Age");
-        int age = scan.nextInt();
-        System.out.println("Age Entered is "+age);
+        try {
+            System.out.println("Enter Your Age");
+            int age = scan.nextInt();
+            System.out.println("Age Entered is "+age);
+        } catch (Exception e) {
+            System.out.println("Age can't be string. Kindly provide integer");
+        }
         System.out.println("Connection terminated Gracefully");
     }
 }

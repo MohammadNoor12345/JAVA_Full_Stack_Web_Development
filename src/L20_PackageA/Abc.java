ceae0584bd6d61573fa945b9730fa787768f0d1b
Mohammad Noor <mohammadnoor76354@gmail.com>
2026-03-05 12:25:18 +0530

    Update-4.4

--- src/L20_PackageA/Abc.java
+++ src/L20_PackageA/Abc.java
@@ -15,7 +15,7 @@ public class Abc{
 class Efg extends Abc{
     @Override
     public void m1() {
-        System.out.println(a);
+//        System.out.println(a);
         System.out.println(b);
         System.out.println(c);
         System.out.println(d);
@@ -28,15 +28,15 @@ class Efg extends Abc{
 //    }
 //}
 
-//Rule of Method Overriding =>
-//1. Child class overriden methods should maintain the same return typer as
-// the parent method as per the primitive data type is considered
-//2. Child class overriden method has to maintain the same access modifier
-// as in the parent. If its needs to change it can increase the visibility
-// but it canot decrease.
+/* Rule of Method Overriding =>
+1. Child class overriden methods should maintain the same return typer as
+ the parent method as per the primitive data type is considered
+2. Child class overriden method has to maintain the same access modifier
+ as in the parent. If its needs to change it can increase the visibility
+ but it canot decrease.
 
 
-// Scope Increase          PRIVATE                 A
-//      |               P A C K A G E           |
-//      |             P R O T E C T E D         |
-//      V               P U B L I C           Scope Decrease
+ Scope Increase          PRIVATE              A
+      |               P A C K A G E           |
+      |             P R O T E C T E D         |
+      V               P U B L I C           Scope Decrease */
--- src/L22_PackageC/L01_XYZ.java
+++ src/L22_PackageC/L01_XYZ.java
@@ -5,9 +5,9 @@ import L20_PackageA.Abc;
 public class XYZ {
     public void m2() {
         Abc b = new Abc();
-        System.out.println(b.a);
-        System.out.println(b.b);
-        System.out.println(b.c);
+//        System.out.println(b.a);
+//        System.out.println(b.b);
+//        System.out.println(b.c);
         System.out.println(b.d);
     }
 }

09766999dd9408641873412d9dbd374e8af6baac
Mohammad Noor <mohammadnoor76354@gmail.com>
2026-03-05 10:43:13 +0530

    Update-4.1

--- src/L19_MyInterface/L04_MyRules.java
+++ src/L19_MyInterface/L04_MyRules.java
@@ -1,6 +1,6 @@
 package L19_MyInterface;
 class MyParent{
-    public int add() {
+    public double add() {
         int a = 10, b = 20;
         System.out.println("a+b = " + (a+b));
         return  (a+b);
@@ -9,16 +9,27 @@ class MyParent{
 }
 class MyChild extends MyParent{
     @Override
-    public int add() {
+    public double add() {
         int a = 100, b = 200;
         System.out.println("a+b = " + (a+b));
         return  (a+b);
     }
 }
+class Animal {
+
+}
+class Lion extends Animal{
+
+}
+class Tiger{
+
+}
 
 public class L04_MyRules {
     static void main(String[] args) {
-        MyChild n = new MyChild();
-        n.add();
+//        MyChild n = new MyChild();
+//        n.add();
+        MyChild m = new MyChild();
+        m.add();
     }
 }

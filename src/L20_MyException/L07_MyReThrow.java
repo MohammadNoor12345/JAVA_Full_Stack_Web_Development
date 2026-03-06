16eb5d8e740e77e0700c5223f505a7bf7838be0d
Mohammad Noor <mohammadnoor76354@gmail.com>
2026-03-06 16:17:53 +0530

    Update-6.2

--- src/L20_MyException/L07_MyReThrow.java
+++ src/L20_MyException/L07_MyReThrow.java
@@ -7,23 +7,42 @@ class MyATM{
         Scanner sc = new Scanner(System.in);
         System.out.println("Connection established");
         try {
+
+            // Asking user to enter first number
             System.out.println("Enter a:");
             int a = sc.nextInt();
+
+            // Asking user to enter second number
             System.out.println("Enter b:");
             int b = sc.nextInt();
-            int c=a/b;
-            System.out.println("Result of a/b is "+c);
+
+            // Risky statement
+            // If b = 0 → ArithmeticException will occur
+            int c = a / b;
+
+            // If no exception occurs, result will print
+            System.out.println("Result of a/b is " + c);
         }
+
         catch (Exception e) {
-            System.out.println("Sorry for incovenience. we ll get back soon");
+
+            // This block executes if any exception occurs in try block
+            System.out.println("Sorry for inconvenience. we ll get back soon");
+
+            // Rethrowing the exception
+            // Exception is sent back to JVM or calling method
             throw e;
         }
+
         finally {
+
+            // finally block always executes
+            // Whether exception occurs or not
             System.out.println("Connection terminated");
+
+            // Closing the Scanner to release resources
             sc.close();
         }
-
-
     }
 
 }

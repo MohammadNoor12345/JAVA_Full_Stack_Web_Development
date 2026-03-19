15e5077814d3f31e1a5ead4ac078fde2c5706be7
Mohammad Noor <mohammadnoor76354@gmail.com>
2026-03-19 12:28:27 +0530

    Update-6.8

---
+++ src/L21_Error/L01_MyError.java
@@ -0,0 +1,8 @@
+package L21_Error;
+
+public class L01_MyError {
+    static void main(String[] args) {
+        int[] a = new int[Integer.MAX_VALUE];
+        System.out.println(a.length);
+    }
+}

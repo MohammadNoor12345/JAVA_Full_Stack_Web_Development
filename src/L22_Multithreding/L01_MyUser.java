9db9f87ef21a619d431014ee496cb239f872a5f4
Mohammad Noor <mohammadnoor76354@gmail.com>
2026-03-24 10:57:12 +0530

    Update-7.1

---
+++ src/L22_Multithreding/L01_MyUser.java
@@ -0,0 +1,31 @@
+package L22_Multithreding;
+class MyDemo extends Thread{
+
+    @Override
+    public void run() {
+
+        for(int i=1;i<=5;i++) {
+            try {
+                System.out.println(i);
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+}
+
+public class L01_MyUser {
+
+    public static void main(String[] args) {
+        System.out.println("Main thread started");
+        MyDemo u1 = new MyDemo();
+        u1.start();
+        System.out.println("Main thread ended");
+
+    }
+
+}

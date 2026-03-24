8e3c44f8c25f921c54aa1aebe6a979ea865c792d
Mohammad Noor <mohammadnoor76354@gmail.com>
2026-03-24 12:12:06 +0530

    Update-7.4

---
+++ src/L22_Multithreding/L03_RaceCondition_for_SharedResource.java
@@ -0,0 +1,42 @@
+package L22_Multithreding;
+
+class WhiteBoard implements Runnable {
+    public void run() {
+        String name = Thread.currentThread().getName();
+        try {
+            System.out.println(name + " trying to access white board");
+            Thread.sleep(1000);
+
+            System.out.println(name + " got access to white board");
+
+            for (int i = 1; i <= 5; i++) {
+                System.out.println(name + " writing on the board");
+                Thread.sleep(2000);
+            }
+
+            System.out.println(name + " done accessing white board");
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+public class L03_RaceCondition_for_SharedResource {
+    public static void main(String[] args) {
+
+        WhiteBoard w = new WhiteBoard(); // ONE shared resource
+
+        Thread t1 = new Thread(w);
+        Thread t2 = new Thread(w);
+        Thread t3 = new Thread(w);
+
+        t1.setName("JAVA");
+        t2.setName("AWS");
+        t3.setName("CYBER");
+
+        t1.start();
+        t2.start();
+        t3.start();
+    }
+}

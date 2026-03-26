3e402533a46569295c4f506ea5fea1ebdef25adb
Mohammad Noor <mohammadnoor76354@gmail.com>
2026-03-26 11:05:08 +0530

    Update-9.6

---
+++ src/L24_Lists/L02_MyArrayList.java
@@ -0,0 +1,41 @@
+package L24_Lists;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class L02_MyArrayList {
+    public static void main(String[] args) {
+
+        ArrayList<Integer> al = new ArrayList<>();
+
+        al.add(10);
+        al.add(20);
+        al.add(30);
+        // al.add("Odisha"); // ❌ Not allowed (Integer list)
+
+        al.add(40);
+        al.add(50);
+        al.add(60);
+        al.add(70);
+        al.add(80);
+        al.add(90);
+        al.add(100);
+
+        System.out.println(al);
+
+        System.out.println();
+        System.out.println("Looping using foreach loop");
+
+        for (Integer a : al) {
+            System.out.println(a);
+        }
+
+        System.out.println();
+        System.out.println("Using iterator");
+
+        Iterator<Integer> itr = al.iterator();
+        while (itr.hasNext()) {
+            System.out.println(itr.next());
+        }
+    }
+}

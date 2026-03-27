21223c64f7f718efa520a513f914fd1a73d0f5ba
Mohammad Noor <mohammadnoor76354@gmail.com>
2026-03-27 10:55:18 +0530

    Update-10.2

---
+++ src/L24_List_In_Array/L01_MyArrayList.java
@@ -0,0 +1,38 @@
+package L24_List_In_Array;
+
+import java.util.ArrayList;
+
+public class L01_MyArrayList {
+    static void main(String[] args) {
+        /*
+         * ArrayList is backed by array
+         * it maintains insertion order (Insertion order is preserved)
+         * Array List is dynamic
+         * it allows duplicate
+         * it allows heterogenous type of data
+         * it allows null values
+         */
+
+        ArrayList al = new ArrayList();
+        al.add(10);
+        al.add(20);
+        al.add(30);
+
+        System.out.println(al);
+        al.add(40);
+        al.add(50);
+        System.out.println(al);
+
+        al.add("Rooman");
+        al.add(30);
+        al.add(40);
+        al.add(null);
+        al.add(null);
+        System.out.println(al);
+        System.out.println();
+        System.out.println("Iterating over list using for");
+        for(int i = 0; i < al.size(); i++){
+            System.out.println(al.get(i));
+        }
+    }
+}

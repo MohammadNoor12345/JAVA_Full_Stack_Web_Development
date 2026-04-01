c7e5f5defb8f377f084ad9c15741b568fa77d9f6
Mohammad Noor <mohammadnoor76354@gmail.com>
2026-04-01 11:17:58 +0530

    Update-11.2

---
+++ src/L26_Functional_Interface/L03_MyMain.java
@@ -0,0 +1,12 @@
+package L26_Functional_Interface;
+interface Calc{
+    int mul(int a, int b);
+}
+
+public class L03_MyMain {
+    static void main(String[] args) {
+        Calc m1 = (int a, int b) -> a * b;
+        int res = m1.mul(10, 20);
+        System.out.println("Multiplication is: " + res);
+    }
+}

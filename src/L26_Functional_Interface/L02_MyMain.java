d0f716e2a781526d48bf0e230674d76487c5f0e0
Mohammad Noor <mohammadnoor76354@gmail.com>
2026-04-01 11:13:48 +0530

    Update-11.1

--- src/L26_Functional_Interface/L02_MyMain.java
+++ src/L26_Functional_Interface/L02_MyMain.java
@@ -6,12 +6,13 @@ interface Opns1 {
 
 public class L02_MyMain {
     public static void main(String[] args) {
+        Opns1 m1 = (int a, int b) -> a + b;
 
-        Opns1 m1 = (int a, int b) -> {
-            int c = a + b;
-            return c;
-        };
-
+//        Opns1 m1 = (int a, int b) -> {
+//            int c = a + b;
+//            return c;
+//        };
+//
         int res = m1.add(100, 200);
         System.out.println(res);
     }

3a896c7be8e8ed9667108535cd1725350d32ec5b
Mohammad Noor <mohammadnoor76354@gmail.com>
2026-04-01 11:08:56 +0530

    Update-11.0

---
+++ src/L26_Functional_Interface/L02_MyMain.java
@@ -0,0 +1,18 @@
+package L26_Functional_Interface;
+
+interface Opns1 {
+    int add(int a, int b);
+}
+
+public class L02_MyMain {
+    public static void main(String[] args) {
+
+        Opns1 m1 = (int a, int b) -> {
+            int c = a + b;
+            return c;
+        };
+
+        int res = m1.add(100, 200);
+        System.out.println(res);
+    }
+}
